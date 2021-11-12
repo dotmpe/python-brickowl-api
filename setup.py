40c69e7806aafd1b40b9e19f138ef3f9c9f567c5
Alexander Schillemans <alexander.schillemans@lhs.global>
2021-11-12 11:18:01 +0100

    Prep for v0.1.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
   name = 'python-brickowl-api',         
   packages=['brickowl', 'brickowl.models', 'brickowl.constants', 'brickowl.endpoints'],
-  version = '0.1.2',
+  version = '0.1.3',
   license='GPL-3.0-or-later',
   description = 'Wrapper for the BrickOwl API (v1)',
   long_description=long_description,
@@ -17,7 +17,7 @@ setup(
   author = 'Alexander Schillemans',
   author_email = 'alexander.schillemans@lhs.global',
   url = 'https://github.com/alexanderlhsglobal/python-brickowl-api',
-  download_url = 'https://github.com/alexanderlhsglobal/python-brickowl-api/archive/refs/tags/0.1.2.tar.gz',
+  download_url = 'https://github.com/alexanderlhsglobal/python-brickowl-api/archive/refs/tags/0.1.3.tar.gz',
   keywords = ['brickowl', 'brick', 'owl', 'brick owl', 'lego', 'api'],
   install_requires=[
           'requests',
